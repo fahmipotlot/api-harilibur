d15d802a563fe66e8dbcdf43ca0a4296ae6b2272
Fahmi Amiruddin Nafi <amirfahmi8@gmail.com>
2024-01-05 15:27:01 +0700

    change source

--- services/scraper.js
+++ services/scraper.js
@@ -6,8 +6,8 @@ import { parseHTML } from 'linkedom';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getHTMLCalendar = async (month, year) => {
-    const response = await fetch(`https://kalenderbali.com?bl=${month}&th=${year}`);
+const getHTMLCalendar = async (year) => {
+    const response = await fetch(`https://publicholidays.co.id/id/${year}-dates/`);
     if (!response.ok) {
         throw new Error(`An error has occured: ${response.status}`);
     }
@@ -19,57 +19,60 @@ function zeroPad(num) {
     return (num.toString().length === 1) ? "0" + num : num;
 }
 
-let keywordFakultatif = ['Galungan', 'Kuningan', 'Siwa Ratri', 'Saraswati'];
-
-async function getDaftarHariLibur(year) {
+async function getHolidayList(year) {
     try {
         let result = [];
-        for (let x = 1; x <= 12; x ++) {
-            console.log(`Mengambil daftar hari libur bulan ke - ${x} tahun ${year}...`);
-            await getHTMLCalendar(x, year).then((data) => {
-                const { document } = parseHTML(data);
-                let waraList = document.querySelectorAll('div.waralist');
-                let daftarHariPenting = waraList[1];
+        console.log(`Mengambil daftar hari libur tahun ${year}...`);
+        await getHTMLCalendar(year).then((data) => {
+            const { document } = parseHTML(data);
+            let publicholidays = document.querySelector("tbody").closest("table.publicholidays");
 
-                // Daftar hari libur
-                let daftarHariLibur = daftarHariPenting.querySelectorAll('div[style="padding-bottom:7px; color:#FF0000"]');
-                Array.from(daftarHariLibur).forEach(e => {
-                    let textContent = e.textContent;
-                    let splitTextContent = textContent.split(".");
-                    let trimSplitTextContent = splitTextContent.map((s) => String.prototype.trim.apply(s));
+            // Daftar hari libur
+            let holidayList1 = publicholidays.querySelectorAll('tbody>tr.odd');
+            let holidayList2 = publicholidays.querySelectorAll('tbody>tr.even');
+            let holidayList = holidayList1.concat(holidayList2);
+
+            Array.from(holidayList).forEach(e => {
+                let td = e.querySelectorAll('td');
+                let dates = td[0]?.textContent;
+                let description = td[2]?.querySelector('a')?.textContent ?? td[2]?.querySelector('span')?.textContent;
+
+                // exclude cuti
+                if (! description.toLowerCase().includes('cuti')) {
+                    // parse dates
+                    let month = dates.split(" ")[1];
+                    let date = dates.split(" ")[0];
+                    switch (month) {
+                        case "Januari": month = '01'; break;
+                        case "Februari": month = '02'; break;
+                        case "Maret": month = '03'; break;
+                        case "April": month = '04'; break;
+                        case "Mei": month = '05'; break;
+                        case "Juni": month = '06'; break;
+                        case "Juli": month = '07'; break;
+                        case "Agustus": month = '08'; break;
+                        case "September": month = '09'; break;
+                        case "Oktober": month = '10'; break;
+                        case "November": month = '11'; break;
+                        case "Desember": month = '12'; break;
+                    }
+
                     let item = {
-                        holiday_date: `${year}-${zeroPad(x)}-${trimSplitTextContent[0]}`,
-                        formatted_holiday_date: new Date(`${year}-${zeroPad(x)}-${trimSplitTextContent[0]}`),
-                        holiday_name: trimSplitTextContent[1],
+                        holiday_dates: `${year}-${month}-${zeroPad(date)}`,
+                        formatted_holiday_dates: new Date(`${year}-${month}-${zeroPad(date)}`),
+                        holiday_name: description,
                         is_national_holiday: true
                     };
-                    result.push(item);
-                });
 
-                // Daftar hari lainnya
-                let daftarHariLainnya = daftarHariPenting.querySelectorAll('div[style="padding-bottom:7px"]');
-                Array.from(daftarHariLainnya).forEach(e => {
-                    let textContent = e.textContent;
-                    let splitTextContent = textContent.split(".");
-                    let trimSplitTextContent = splitTextContent.map((s) => String.prototype.trim.apply(s));
-                    for (let key of keywordFakultatif) {
-                        if (trimSplitTextContent[1].includes(key)) {
-                            let item = {
-                                holiday_date: `${year}-${zeroPad(x)}-${trimSplitTextContent[0]}`,
-                                formatted_holiday_date: new Date(`${year}-${zeroPad(x)}-${trimSplitTextContent[0]}`),
-                                holiday_name: trimSplitTextContent[1],
-                                is_national_holiday: false
-                            };
-                            result.push(item);
-                        }
-                    }                        
-                });
-            })
-        }
-        // See: https://flaviocopes.com/how-to-sort-array-by-date-javascript/
-        const sortedResult = result.slice().sort((a, b) => b.formatted_holiday_date - a.formatted_holiday_date);
+                    result.push(item);
+                }
+            });
+        })
+
+        // See: https://flaviocopes.com/how-to-sort-array-by-dates-javascript/
+        const sortedResult = result.slice().sort((a, b) => b.formatted_holiday_dates - a.formatted_holiday_dates);
         for (const item of sortedResult) {
-            delete item.formatted_holiday_date;
+            delete item.formatted_holiday_dates;
         }
 
         fs.writeFile(outputFile, JSON.stringify(sortedResult), err => {
@@ -96,4 +99,4 @@ if (myArgs.length > 0) {
 
 const outputFile = path.join(__dirname, '..', 'data', `${year}.json`);
 
-(async () => await getDaftarHariLibur(year))();
+(async () => await getHolidayList(year))();
