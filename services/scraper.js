63c1a9ccf9d04f0ed905ed9d36d1f51ec6c1993f
Fahmi Amiruddin Nafi <amirfahmi8@gmail.com>
2024-01-05 17:36:40 +0700

    log

--- services/scraper.js
+++ services/scraper.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getHTMLCalendar = async (year) => {
-    const response = await fetch(`https://publicholidays.co.id/id/${year}-datesdsad/`, {
+    const response = await fetch(`https://publicholidays.co.id/id/${year}-dates/`, {
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
         }
