cfafbf798f4a4643a0758370cc21a77122290741
Fahmi Amiruddin Nafi <amirfahmi8@gmail.com>
2024-01-08 08:14:48 +0700

    user agent

--- services/1.js
+++ services/1.js
@@ -1,14 +1,16 @@
 import puppeteer from 'puppeteer-extra'
+import userAgent from 'user-agents';
 // import pluginStealth from 'puppeteer-extra-plugin-stealth'
 
 (async () => {
     let headless = true;
     const browser = await puppeteer.launch({headless});
     const page = await browser.newPage();
-    await page.setJavaScriptEnabled(false);
+    await page.setUserAgent(userAgent.random().toString())
+    await page.setJavaScriptEnabled(true);
   
-    await page.goto('https://publicholidays.co.id/id/2024-dates/');
-    // await page.goto('https://www.g2.com/');
+    // await page.goto('https://publicholidays.co.id/id/2024-dates/');
+    await page.goto('https://www.g2.com/');
     await page.screenshot({path: 'out.png'});
   
     const html = await page.content();
