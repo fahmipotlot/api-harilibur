04922f414f40ae1a7122bcf279b77a29bba76e6c
Fahmi Amiruddin Nafi <amirfahmi8@gmail.com>
2024-01-08 13:56:58 +0700

    stealt

--- services/1.js
+++ services/1.js
@@ -1,9 +1,11 @@
 import puppeteer from 'puppeteer-extra'
 import userAgent from 'user-agents';
+import pluginStealth from 'puppeteer-extra-plugin-stealth';
 // import pluginStealth from 'puppeteer-extra-plugin-stealth'
 
 (async () => {
     let headless = true;
+    puppeteer.use(pluginStealth())
     const browser = await puppeteer.launch({headless});
     const page = await browser.newPage();
     await page.setUserAgent(userAgent.random().toString())
